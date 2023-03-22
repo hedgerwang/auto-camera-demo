9c4eb912a0471ae7407b7f72c69dc73f6d80ad0c
Hedger Wang <hedger@stripe.com>
2023-03-21 21:18:01 -0700

    build 2023-03-21-21-18

--- publish.sh
+++ publish.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+cp ../idxp-camera-app/build/*.js build/
+cp -fr ../idxp-camera-app/build/advanced  build/
+cp -fr ../idxp-camera-app/build/advanced-threads  build/
+
 node scripts/publish.js 
 git add -A .
+cp 
 timestamp=$(date '+%Y-%m-%d-%H-%M')
 git commit -m "build $timestamp"
 git push origin master
